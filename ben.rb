d0678398439cbebc7658f49018786a8b8758c01d
Muhammet Yildiz <muhammetyildiz@gmail.com>
2020-04-19 20:11:36 +0000

    Done.

---
+++ ben.rb
@@ -0,0 +1,13 @@
+# don't forget to add: require 'pry'
+require 'pry'
+
+def selection(num)
+  if num == 1
+  puts "YUM YUM MUNCH MUNCH MUNCH"
+elsif num == 2
+  puts "HAM HAM HAM IN MY TUMMY"
+  end
+
+end
+
+selection 2
